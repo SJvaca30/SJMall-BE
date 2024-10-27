b5c98e0db02560d681729a487bbae6e5b1f78152
SJvaca30 <yyvaca30@gmail.com>
2024-10-27 21:14:02 +0900

    feat: token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const authController = {};
 
 authController.loginWithEmail = async (req, res) => {
@@ -24,6 +27,31 @@ authController.loginWithEmail = async (req, res) => {
       status: "fail",
       error: "이메일 또는 비밀번호가 일치하지 않습니다.",
     });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) {
+      return res.status(400).json({
+        status: "fail",
+        error: "토큰이 없습니다.",
+      });
+    }
+    const token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+      if (error) {
+        return res.status(400).json({
+          status: "fail",
+          error: "토큰 jwt.verify에 실패했습니다.",
+        });
+      }
+      req.userId = payload._id;
+    });
+    next(); // authController.authenticate 끝나고 userController.getUser 실행
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
